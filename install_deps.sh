f0b62ce3867ded3334df1163d44af682fe6c3f54
Sho Okumoto <lebesgue0118@gmail.com>
2024-07-13 04:14:54 +0900

    依存関係インストール用スクリプトの作成

---
+++ install_deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e -x
+
+sudo apt-get update
+
+# OpenGL
+sudo apt-get install cmake pkg-config
+sudo apt-get install mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev 
+sudo apt-get install libglew-dev libglfw3-dev libglm-dev 
+sudo apt-get install libao-dev libmpg123-dev
+
