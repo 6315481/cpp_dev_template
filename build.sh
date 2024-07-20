b575c2b3142298d068bb44592a2ccaafb9e5a022
Sho Okumoto <lebesgue0118@gmail.com>
2024-07-20 18:36:50 +0900

    パスの設定バグ修正

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e -x
 
 BUILD_DIR="build"
 BUILD_TARGET="all"
+BUILD_TYPE="Debug"
 
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir $BUILD_DIR
@@ -20,6 +21,10 @@ while [ $# -gt 0 ]; do
             BUILD_TARGET="$1"
             shift
             ;;
+        "--release")
+            shift
+            BUILD_TYPE="Release"
+            ;;
         "--not-install")
             NOT_INSTALL="ON"
             shift
@@ -31,7 +36,7 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-cmake -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=./bin
+cmake -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=./bin -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 cmake --build $BUILD_DIR --target $BUILD_TARGET -j$(nproc)
 if [ !-z "${NOT_INSTALL}"]; then
     cmake --install $BUILD_DIR --component $BUILD_TARGET
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,104 @@
 #include <iostream>
-#include <span>
-#include <string>
-#include <filesystem>
-#include <sys/socket.h>
 
-#include "sample.hpp"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <fmt/format.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
+    GLFWwindow* window;
 
-    hello_world();
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(640, 480, "Hello world", nullptr, nullptr);
+    if (!window) {
+        std::cout << "失敗" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    int platform = glfwGetPlatform();
+
+    glfwMakeContextCurrent(window);
+
+    const int version = gladLoadGL();
+    if (version == 0) {
+        fprintf(stderr, "Failed to load OpenGL 3.x/4.x libraries!\n");
+        return 1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    bool show_demo_window, show_another_window;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, World");
+            ImGui::Text("This is some useful text");
+            ImGui::Checkbox("Demo Window", &show_demo_window);
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("Clear Color", reinterpret_cast<float*>(&clear_color));
+
+            if (ImGui::Button("Button")) {
+                counter ++;
+            }
+
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
